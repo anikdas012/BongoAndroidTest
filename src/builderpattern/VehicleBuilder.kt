af89dfc0c4f9118a9394b5c6798315551d8d55a9
Anik Das <anikdas012@gmail.com>
2020-08-05 01:35:04 +0600

    Add method to get vehicle type

--- src/builderpattern/VehicleBuilder.kt
+++ src/builderpattern/VehicleBuilder.kt
@@ -1,4 +1,10 @@
 package builderpattern
 
 class VehicleBuilder {
+    private lateinit var vehicleType: String
+
+    fun vehicleType(type: String): VehicleBuilder {
+        vehicleType = type
+        return this
+    }
 }
