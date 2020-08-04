4a22bb81bf114402a495ca755866850f071ba79e
Anik Das <anikdas012@gmail.com>
2020-08-05 01:36:58 +0600

    Add method to get instance using VehicleBuilder

--- src/builderpattern/BuilderDemo.kt
+++ src/builderpattern/BuilderDemo.kt
@@ -1,2 +1,13 @@
 package builderpattern
 
+fun main() {
+    val car: Vehicle = VehicleBuilder().vehicleType("car").build()!!
+    println("Car wheel numbers: ${car.set_num_of_wheels()}")
+    println("Car passenger numbers: ${car.set_num_of_passengers()}")
+    println("Car has gas: ${car.has_gas()}")
+
+    val plane: Vehicle = VehicleBuilder().vehicleType("plane").build()!!
+    println("Plane wheel numbers: ${plane.set_num_of_wheels()}")
+    println("Plane passenger numbers: ${plane.set_num_of_passengers()}")
+    println("Plane has gas: ${plane.has_gas()}")
+}
