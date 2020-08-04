41c16cd15400d481a5f6186134a2e7a1ac3e916e
Anik Das <anikdas012@gmail.com>
2020-08-05 01:24:56 +0600

    Override necessary methods of Vehicle interface

--- src/factorypattern/Plane.kt
+++ src/factorypattern/Plane.kt
@@ -1,5 +1,18 @@
 package factorypattern
 
 class Plane(private var gas: Boolean = true, private var passengerNum: Int = 180, private var wheelNum: Int = 10): Vehicle {
+    override fun set_num_of_wheels(): Int {
+        println("Plane set_num_of_wheels called")
+        return wheelNum
+    }
 
+    override fun set_num_of_passengers(): Int {
+        println("Plane set_num_of_passengers called")
+        return passengerNum
+    }
+
+    override fun has_gas(): Boolean {
+        println("Plane has_gas called")
+        return gas
+    }
 }
