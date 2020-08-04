913d7451054569697ac5071ba11b718071d63e19
Anik Das <anikdas012@gmail.com>
2020-08-05 01:28:56 +0600

    Add method to get Car and Plane instance using VehicleFactory class

--- src/factorypattern/FactoryDemo.kt
+++ src/factorypattern/FactoryDemo.kt
@@ -1,2 +1,13 @@
 package factorypattern
 
+fun main() {
+    val car: Vehicle = VehicleFactory().getVehicle("car")!!
+    println("Car wheel numbers: ${car.set_num_of_wheels()}")
+    println("Car passenger numbers: ${car.set_num_of_passengers()}")
+    println("Car has gas: ${car.has_gas()}")
+
+    val plane: Vehicle = VehicleFactory().getVehicle("plane")!!
+    println("Plane wheel numbers: ${plane.set_num_of_wheels()}")
+    println("Plane passenger numbers: ${plane.set_num_of_passengers()}")
+    println("Plane has gas: ${plane.has_gas()}")
+}
