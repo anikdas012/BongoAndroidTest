894acac4956987dd06902fec967734f9691dbf9c
Anik Das <anikdas012@gmail.com>
2020-08-05 01:24:00 +0600

    Override necessary methods

--- src/factorypattern/Car.kt
+++ src/factorypattern/Car.kt
@@ -1,5 +1,18 @@
 package factorypattern
 
 class Car(private var gas: Boolean = false, private var passengerNum: Int = 3, private var wheelNum: Int = 4): Vehicle {
-    
+    override fun set_num_of_wheels(): Int {
+        println("Car set_num_of_wheels called")
+        return wheelNum
+    }
+
+    override fun set_num_of_passengers(): Int {
+        println("Car set_num_of_passengers called")
+        return passengerNum
+    }
+
+    override fun has_gas(): Boolean {
+        println("Car has_gas called")
+        return gas
+    }
 }
