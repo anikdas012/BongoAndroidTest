26b66fe39742b3d532ce80551506ecbf3f9986b1
Anik Das <anikdas012@gmail.com>
2020-08-05 00:53:30 +0600

    Return false if both strings are not of same length

--- src/anagram/Anagram.kt
+++ src/anagram/Anagram.kt
@@ -3,4 +3,7 @@ package anagram
 fun isAnagram(stringOne: String, stringTwo: String): Boolean {
     val stringOne = stringOne.replace(" ", "")
     val stringTwo = stringTwo.replace(" ", "")
+    if (stringOne.length != stringTwo.length) {
+        return false
+    }
 }
