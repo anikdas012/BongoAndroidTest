46382df46733c9787c0975643e7ae992718a2886
Anik Das <anikdas012@gmail.com>
2020-08-05 00:52:11 +0600

    Replace empty spaces from the input strings

--- src/anagram/Anagram.kt
+++ src/anagram/Anagram.kt
@@ -1,5 +1,6 @@
 package anagram
 
 fun isAnagram(stringOne: String, stringTwo: String): Boolean {
-    
+    val stringOne = stringOne.replace(" ", "")
+    val stringTwo = stringTwo.replace(" ", "")
 }
