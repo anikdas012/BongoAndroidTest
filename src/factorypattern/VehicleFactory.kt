4b7dbb082751cf1e31413419a7e1b5f32fdf5c78
Anik Das <anikdas012@gmail.com>
2020-08-05 01:27:29 +0600

    Add method to provide Car and Plane instance

--- src/factorypattern/VehicleFactory.kt
+++ src/factorypattern/VehicleFactory.kt
@@ -1,5 +1,11 @@
 package factorypattern
 
 class VehicleFactory {
-    
+    fun getVehicle(type: String): Vehicle? {
+        when(type.toLowerCase()) {
+            "car" -> return Car()
+            "plane" -> return Plane()
+        }
+        return null
+    }
 }
